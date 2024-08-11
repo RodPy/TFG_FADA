2159d7717e5fabd14e00c787211b9dde1ce8ed86
Rodney <rodneyrrspy@gmail.com>
2024-08-10 21:15:40 -0400

    add full led

--- ArquiTFG/ArquiTFG.ino
+++ ArquiTFG/ArquiTFG.ino
@@ -14,8 +14,8 @@
 //#define BTN_2     2 // On Trinket or Gemma, suggest changing this to 1
 //#define BTN_3     3 // On Trinket or Gemma, suggest changing this to 1
 
-#define LED        5 // On Trinket or Gemma, suggest changing this to 1
-#define LED        8\  // On Trinket or Gemma, suggest changing this to 1
+#define LED        8 // On Trinket or Gemma, suggest changing this to 1
+#define LED2        5  // On Trinket or Gemma, suggest changing this to 1
 
 #define sw        4 // On Trinket or Gemma, suggest changing this to 1
 
@@ -24,9 +24,11 @@
 #define BTN_2     3 // On Trinket or Gemma, suggest changing this to 1
 #define BTN_3     2 // On Trinket or Gemma, suggest changing this to 1
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 67 // Popular NeoPixel ring size
+#define NUMPIXELS 81 // Popular NeoPixel ring size
+#define NUMPIXELS2 67 // Popular NeoPixel ring size
 
 Adafruit_NeoPixel pixels(NUMPIXELS, LED, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels2(NUMPIXELS2, LED2, NEO_GRB + NEO_KHZ800);
 
 #define DELAYVAL 50 // Time (in milliseconds) to pause between pixels
  bool bandera = false;
@@ -45,11 +47,10 @@ void setup() {
   pinMode(sw , INPUT);
 
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels2.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 
   Serial.begin(9600);
     attachInterrupt(digitalPinToInterrupt(sw), blink, CHANGE);
-
-
   // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
   // Any other board, you can remove this part (but no harm leaving it):
 
@@ -57,7 +58,8 @@ void setup() {
   clock_prescale_set(clock_div_1);
 #endif
   // END of Trinket-specific code.
-  rainbow(1);
+  rainbow(0.5);
+  rainbow2(0.5);
 
 }
 
@@ -65,11 +67,29 @@ int led = 0;
 
 void secuenciaLed(int R, int G, int B, int timeDelay) {
 
+  for (int i = 0; i <= NUMPIXELS ; i++) {
+    // led = NUMPIXELS - i;
+    pixels.setPixelColor(i, pixels.Color(R, G, B));
+        pixels2.setPixelColor(i, pixels2.Color(R, G, B));
+
+    // pixels.setPixelColor(led, pixels.Color(R, G, B));
+    //     pixels2.setPixelColor(led, pixels2.Color(R, G, B));
+
+    pixels.show();   // Send the updated pixel colors to the hardware.
+        pixels2.show();   // Send the updated pixel colors to the hardware.
+
+    delay(timeDelay); // Pause before next pass through loop
+  }
+
+}
+
+void secuenciaLed2(int R, int G, int B, int timeDelay) {
+
   for (int i = 0; i <= NUMPIXELS / 2; i++) {
     led = NUMPIXELS - i;
-    pixels.setPixelColor(i, pixels.Color(R, G, B));
-    pixels.setPixelColor(led, pixels.Color(R, G, B));
-    pixels.show();   // Send the updated pixel colors to the hardware.
+    pixels2.setPixelColor(i, pixels2.Color(R, G, B));
+    pixels2.setPixelColor(led, pixels2.Color(R, G, B));
+    pixels2.show();   // Send the updated pixel colors to the hardware.
     delay(timeDelay); // Pause before next pass through loop
   }
 
@@ -86,16 +106,30 @@ void rainbow(int wait) {
   }
 }
 
+void rainbow2(int wait) {
+  for (long firstPixelHue = 0; firstPixelHue < 5 * 65536; firstPixelHue += 256) {
+    for (int i = 0; i <= NUMPIXELS2; i++) {
+      int pixelHue = firstPixelHue + (i * 65536L / pixels2.numPixels());
+      pixels2.setPixelColor(i, pixels2.gamma32(pixels2.ColorHSV(pixelHue)));
+    }
+    pixels2.show();
+    delay(wait);
+  }
+}
+
 void loop() {
 
   pixels.clear(); // Set all pixel colors to 'off'
+  pixels2.clear(); // Set all pixel colors to 'off'
 
 
   // Apagado
   if (digitalRead(BTN_1)) {
     Serial.println("BTN_1 ");
     pixels.setPixelColor(led, pixels.Color(0, 0, 0));
+    pixels2.setPixelColor(led, pixels.Color(0, 0, 0));
     pixels.show();   // Send the updated pixel colors to the hardware.
+    pixels2.show();   // Send the updated pixel colors to the hardware.
 
 
   }
@@ -112,13 +146,12 @@ void loop() {
       if (digitalRead(SENSOR)) {
         Serial.println("sensor ");
 
-        secuenciaLed(0, 255, 0, 100);
-
+        secuenciaLed(0, 150, 0, 100);
       }
       else {
         Serial.println("NOsensor ");
 
-        secuenciaLed(255, 0, 0, 100);
+        secuenciaLed(150, 0, 0, 100);
 
 
       }
@@ -129,13 +162,19 @@ void loop() {
 
       for (int i = 0; i <= NUMPIXELS; i++) {
         pixels.setPixelColor(i, pixels.Color(50, 50, 0));
+        pixels2.setPixelColor(i, pixels2.Color(50, 50, 0));
       }
       pixels.show();   // Send the updated pixel colors to the hardware.
+      pixels2.show();   // Send the updated pixel colors to the hardware.
       delay(100);
       for (int i = 0; i <= NUMPIXELS; i++) {
         pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+        pixels2.setPixelColor(i, pixels.Color(0, 0, 0));
+
       }
       pixels.show();   // Send the updated pixel colors to the hardware.
+      pixels2.show();   // Send the updated pixel colors to the hardware.
+
       delay(100);
 
     }
@@ -148,8 +187,10 @@ void loop() {
 
     for (int i = 0; i <= NUMPIXELS; i++) {
       pixels.setPixelColor(i, pixels.Color(50, 50, 50));
+      pixels2.setPixelColor(i, pixels.Color(50, 50, 50));
     }
     pixels.show();   // Send the updated pixel colors to the hardware.
+    pixels2.show();   // Send the updated pixel colors to the hardware.
 
   }
 
